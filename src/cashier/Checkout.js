14595d27eb772bd6dcf4ab72df30a0955d9176b7
Nicholas Gallion <tdng1@unigroupinc.com>
2019-11-10 12:25:47 -0600

    made checkout component functional

--- src/cashier/Checkout.js
+++ src/cashier/Checkout.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 /*
 Hey Saad! I was thinking it would be good to drive home the react stuff, so if you want, building a shopping cart would be a good exercise. Then after you build it, we can add in an api. Heres some requirements to work with:
@@ -13,24 +13,12 @@ Everything else is up to however you want to implement it
 I’d suggest setting up a JSON file with your “inventory”, with just a few items and any other properties those items would need
 */
 
-
-
-
-
-
-class Checkout extends Component {
- 
-
-  render(){
-    return(
-      <div>
-        <h3>subtotal: <span>{this.props.sub}</span></h3>
-        <h3>tax: <span>{this.props.t}</span></h3>
-        <h3>total: <span>{this.props.tot}</span></h3>
-      </div>
-    )
-      
-}
-}
+const Checkout = props => (
+  <div>
+    <h3>subtotal: <span>{props.sub}</span></h3>
+    <h3>tax: <span>{props.t}</span></h3>
+    <h3>total: <span>{props.tot}</span></h3>
+  </div>
+);
 
 export default Checkout;
